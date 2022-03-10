3135203ff60678fec5494ea57597bc88ce85cd05
loumoraes23 <100954902+loumoraes23@users.noreply.github.com>
2022-03-10 18:17:12 -0300

    Add files via upload

---
+++ Primeiro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () 
+{
+    std::cout << "Louise" << std::endl;
+    std::cout << "30 anos" << std::endl;
+    std::cout << "Let it be" << std::endl;
+    system("PAUSE");
+    return 0;
+
+}
